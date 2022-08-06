7a431db1735a5d626af6e362d0c4e93dc5841bf3
Souvik Roy <souvik@consilioussystems.com>
2022-08-06 21:53:49 +0530

    sponsorship payment fields

--- resources/views/admin/sponsorship-payments.blade.php
+++ resources/views/admin/sponsorship-payments.blade.php
@@ -10,13 +10,23 @@
         <table class="table">
             <thead>
                 <tr>
-                    <th class="border-top-0">Title</th>
+                    <th class="border-top-0">User</th>
+                    <th class="border-top-0">Sponsorship</th>
+                    <th class="border-top-0">Transaction ID</th>
+                    <th class="border-top-0">Status</th>
+                    <th class="border-top-0">Amount (INR)</th>
+                    <th class="border-top-0">Date</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($sponsorshipPayments as $sponsorshipPayment)
                 <tr>
-                    <td>{{$sponsorship->title}}</td>
+                    <td>{{$sponsorshipPayment->user->name}}</td>
+                    <td>{{$sponsorshipPayment->sponsorship->title}}</td>
+                    <td>{{$sponsorshipPayment->transaction_id}}</td>
+                    <td>{{$sponsorshipPayment->status}}</td>
+                    <td>{{$sponsorshipPayment->amount}}</td>
+                    <td>{{$sponsorshipPayment->created_at}}</td>
                 </tr>
                 @endforeach
             </tbody>
