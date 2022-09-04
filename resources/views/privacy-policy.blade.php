70a1ce29608adce7a1195a26d94a7fd5ae675144
Souvik Roy <souvik@consilioussystems.com>
2022-09-04 20:06:25 +0530

    add tnd and pp pages

--- resources/views/privacy-policy.blade.php
+++ resources/views/privacy-policy.blade.php
@@ -3,5 +3,60 @@
 
 <h1>Privacy Policy</h1>
 
+<p> Please read all the terms and conditions mentioned below very carefully. By accessing this website and any of the pages thereof, you are deemed to have read and agreed to the terms and conditions below. </p>
+
+<p><strong>Warranty Disclaimers</strong> </p>
+
+<p>
+The information contained in this site, including all text, names, images, pictures, drawings, perspectives, models, graphics, logos, icons, links and other items are provided “as is”. Venous Association Of India does not warrant, represent, undertake, indemnify in any manner whatsoever the accuracy, correctness, adequacy, completeness or suitability of such information and hereby expressly disclaims liability for errors or omissions in such information. </p>
+
+<p>
+No warranty or representation of any kind, expressed, implied or statutory, including but not limited to the warranties or representations of merchantability fitness for a particular purpose, non-infringement and title, is given by Venous Association Of India in conjunction with any information on these pages and any such warranty or representation is hereby expressly excluded. </p>
+
+<p>
+Venous Association Of India also does not warrant that this website and its pages or the server that makes them available are free from computer virus or other corrupting or destructive codes, programs, macros or elements of any kind, nor is any warranty given for the timely, secure, complete, error-free or uninterrupted access to all contents on this website. </p>
+
+<p><strong>Exclusion of Liability</strong> </p>
+
+<p> The access and/or use of any information contained in this website are at the sole risk of the user. Under no circumstances shall Venous Association Of India or any of its subsidiaries, associates, affiliates, marketing partners, agents, suppliers, officers, or employees be liable for any losses, damages, costs and expenses whatsoever (whether direct, indirect, consequential, incidental, special or economic including loss of profits) whether in an action in contract, negligence or tort arising out of or in connection with: </p>
+
+<ul>
+<li style="color:#000;     font-weight: inherit;"> The access or use, inability to access or use, or incomplete, delayed or interrupted, distorted access or use of this website or any other website linked to this website; </li>
+<li style="color:#000;     font-weight: inherit;"> Their reliance on information contained on this website or on any other website linked in to this website.</li>
+<li style="color:#000;     font-weight: inherit;"> The failure of performance, error, omission or defect of any network, line or server system or the transmission to any computer hardware or software used in accessing this website of any computer virus or other corrupting or destructive codes, programs, macros or elements of any kind; or </li>
+<li style="color:#000;     font-weight: inherit;"> The access by any unauthorised person to any information transmitted by you to Venous Association Of India or vice versa through this website. </li>
+</ul>
+
+<p><strong>Links to Third Party Sites</strong> </p>
+
+
+<p>This website may contain links to other websites, which are not under the control of and are not maintained by Venous Association Of India and the contents, opinions and other links provided by these websites are not investigated, verified or monitored by Venous Association Of India, Our Company will not be responsible for the contents, opinions or other links contained in any such linked websites or any changes or updates thereto. Such links is not an endorsement by Venous Association Of India of the website or the contents therein, and you are put on notice that any access by you of such linked websites is at your own risk. </p>
+
+
+<p><strong>Submission of Feedback and Information </strong></p>
+
+<p>Any feedback and information submitted to Venous Association Of India at this website (whether by e-mail or otherwise) shall be deemed to be non-confidential and shall become the property of Venous Association Of India ,who shall be entitled to use such feedback and information on an unrestricted basis for any purpose (including without limitation, reproduction, disclosure to third parties, transmission, publication, broadcast and in relation to the development, marketing and promotion of any facilities, products or services). Please understand that Venous Association Of India shall not be subject to any obligation of confidentiality in relation to, and gives no assurances that any ideas, concepts, know-how, processes or techniques contained in any feedback or information submitted to Venous Association Of India will be treated as confidential or proprietary. </p>
+
+
+
+Copyright @ Venous Association Of India, Bangalore. All Rights Reserved
+
+
+Copyright in the contents of these pages, in the information therein and in their arrangement, is owned by venous association of india unless otherwise indicated. No hyperlinking of this website to any other website or the mirroring reproduction adoption of any information on this website on any other website or the use of Any information therein for any public or commercial purpose is permitted without the prior written consent of Venous Association Of India which consent may be granted or refused at its sole discretion and may be subject to terms and conditions imposed by Venous Association Of India.
+
+
+All information, designs, trademarks, logos, service marks, collective marks, design rights or similar rights that are mentioned in the presentation/attachment/file are the exclusive property of Venous Association Of India. All intellectual property rights including without limitation copyrights and all other rights arising out of the presentation / attachment / file shall vest solely and exclusively with Venous Association Of India.
+
+
+No part of the presentation/attachment/file may be modified, reproduced or distributed in any form or by any means or stored in a database or retrieval system, without the prior written permission of Venous Association Of India, in any network or other electronic storage or transmission, or broadcast for any purpose whatsoever. Venous Association Of India does not guarantee the accuracy, adequacy or completeness of any information or communication mentioned in the presentation/attachment/file and is not responsible, directly or indirectly, for any errors or omissions or for the results obtained from the use of such information or communication.
+
+
+
+
+
+
+
+
+
 
 @stop
