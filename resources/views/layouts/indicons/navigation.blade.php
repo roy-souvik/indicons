fcfc249c94d4088b7ee8d8bf7c03ebd3cec7bf7b
Souvik <souvik0789@gmail.com>
2025-03-09 01:16:02 +0530

    ui updates

--- resources/views/layouts/indicons/navigation.blade.php
+++ resources/views/layouts/indicons/navigation.blade.php
@@ -10,31 +10,24 @@
                     </div>
 
                     <ul class="links">
-                        <!--<li><a href="{{route('home')}}"><i class="fa-solid fa-house"></i></a></li>-->
-                        <li><a href="{{route('home')}}">Home</a></li>
-                        <li><a href="{{route('conference.home')}}">Conference</a></li>
-                        <li><a href="{{route('information.home')}}">Information</a></li>
-                        <li><a href="{{route('scientific.home')}}">Scientific</a></li>
-                        <!--<li>-->
-                        <!--    <a href="#"> Abstracts </a> <i class='bx bxs-chevron-down js-arrow arrow '></i>-->
 
-                        <!--    <ul class="js-sub-menu sub-menu">-->
-                        <!--        <li><a href="{{route('abstract.dates')}}">Important Dates</a></li>-->
-                        <!--        <li><a href="{{route('abstract.guidelines')}}">Abstract Guideline</a></li>-->
-                        <!--        <li><a href="{{route('abstract.posterguidelines')}}">Poster Guideline</a></li>-->
-                        <!--        <li><a href="{{route('abstract.eposterguidelines')}}">E-poster Guideline</a></li>-->
-                        <!--        <li><a href="{{route('abstract.submitpage')}}">Submit Abstract</a></li>-->
-                        <!--    </ul>-->
-                        <!--</li>-->
+                        <li><a href="{{route('home')}}">Home</a></li>
+
+                        <li><a href="{{route('information.home')}}">Information</a></li>
 
                         <li>
-                            <a href="#">Gallery</a> <i class='bx bxs-chevron-down js-arrow arrow '></i>
+                            <a href="#"> Abstracts </a> <i class='bx bxs-chevron-down js-arrow arrow '></i>
 
                             <ul class="js-sub-menu sub-menu">
-                                <li><a href="{{route('gallery.images.categories')}}">Images</a></li>
-                                <li><a href="{{route('gallery.videos.categories')}}">Videos</a></li>
+
+                                <li><a href="{{route('abstract.guidelines')}}">Abstract Guideline</a></li>
+                                <li><a href="{{route('abstract.posterguidelines')}}">Poster Guideline</a></li>
+                                <li><a href="{{route('abstract.eposterguidelines')}}">E-poster Guideline</a></li>
+                                <li><a href="{{route('abstract.submitpage')}}">Submit Abstract</a></li>
                             </ul>
                         </li>
+
+
 
                         @if (Auth::user() && !Auth::user()->isSuperAdmin())
                         <li><a href="/conference-payment">Conference Payment</a></li>
@@ -44,9 +37,7 @@
                         <li><a href="{{route('conference-register.show')}}">Registration</a></li>
                         @endif
                         <li><a href="{{route('accommodation.home')}}">Accommodation</a></li>
-                        <li><a href="{{route('nurses.home')}}">Nursing</a></li>
-                        <!--<li><a target="_blank" href="https://venous.in">VAI</a></li>-->
-                        <li><a href="{{route('sponsorship.show')}}">Sponsorship</a></li>
+
 
                         @if (Auth::user())
                         <li>
